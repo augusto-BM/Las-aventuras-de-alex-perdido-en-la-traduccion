396f0462177827d5eba5838eca25a3783c1757c7
Augusto BM <137666674+augusto-BM@users.noreply.github.com>
2024-10-11 20:07:35 -0500

    culminando efecto parallax del primer escenario

--- Assets/Script/PrimerEscenario/Personajes/personajeMovimiento.cs
+++ Assets/Script/PrimerEscenario/Personajes/personajeMovimiento.cs
@@ -4,7 +4,15 @@ using UnityEngine;
 
 public class personajeMovimiento : MonoBehaviour
 {
-    public float speed = 5f;
+    // Velocidad de movimiento
+    public float velocidad = 5f;
+
+    //Fuerza de salto
+    public float fuerzaSalto = 10f;
+    public float longitudRaycast = 0.1f;
+    public LayerMask capaSuelo;
+    private bool enSuelo;
+
     private Rigidbody2D rb;
 
     // Solo si se quiere hacer animaciones
@@ -20,27 +28,48 @@ public class personajeMovimiento : MonoBehaviour
 
     void Update()
     {
-        float moveInput = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+        /* =================== MOVIMIENTO PERSONAJE =========================== */
+            float moverObjeto = Input.GetAxis("Horizontal");
+            rb.velocity = new Vector2(moverObjeto * velocidad, rb.velocity.y);
 
-        // Ajustar la escala según la dirección del movimiento
-        if (moveInput > 0)
-        {
-            transform.localScale = new Vector3(1, 1, 1);
-        }
-        else if (moveInput < 0)
-        {
-            transform.localScale = new Vector3(-1, 1, 1);
-        }
+            // Ajustar la escala según la dirección del movimiento
+            if (moverObjeto > 0)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+            else if (moverObjeto < 0)
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
+        /* ===================================================================== */
 
-        // Solo si se quiere hacer animaciones
-        /* if (moveInput != 0)
-        {
-            anim.SetBool("estaCorriendo", true);
-        }
-        else
-        {
-            anim.SetBool("estaCorriendo", false);
-        } */
+        /* =================== SALTO PERSONAJE =========================== */
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, longitudRaycast, capaSuelo);
+            enSuelo = hit.collider != null;
+            if(enSuelo && Input.GetKeyDown(KeyCode.Space)){
+                rb.AddForce(new Vector2(0f, fuerzaSalto), ForceMode2D.Impulse);
+            }
+        /* ===================================================================== */
+
+
+        /* =================== ANIMACION PERSONAJE =========================== */
+
+            // Solo si se quiere hacer animaciones
+            /* if (moverObjeto != 0)
+            {
+                anim.SetBool("estaCorriendo", true);
+            }
+            else
+            {
+                anim.SetBool("estaCorriendo", false);
+            } */
+        /* ===================================================================== */
+
+    }
+
+    // Dibujar el raycast en el editor para el salto
+    void OnDrawGizmos(){
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * longitudRaycast);
     }
 }
